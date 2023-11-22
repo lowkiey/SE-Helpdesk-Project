126b24d62082b53147dc787a6d3081f186ff3240
MartinaMaurice <MartinaMaurice.280@gmail.com>
2023-11-22 22:28:33 +0200

    Starter Code

---
+++ Backend/Routes/users.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controller/userController");
+const authorizationMiddleware = require('../Middleware/authorizationMiddleware')
