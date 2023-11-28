7bb83d3634845466faa4ec1306ffddc8777983cd
MartinaMaurice <MartinaMaurice.280@gmail.com>
2023-11-29 00:27:00 +0200

    Last edits fl user

--- Backend/Routes/users.js
+++ Backend/Routes/users.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../Controller/userController"); // Fix casing issue
 const authorizationMiddleware = require('../Middleware/autherizationMiddleware');
+// * Get all users
 router.get("/",  authorizationMiddleware(['admin']),userController.getAllUsers);
-
 // * Get one user
 router.get("/:id", authorizationMiddleware(['admin','customer']), userController.getUser);
 
