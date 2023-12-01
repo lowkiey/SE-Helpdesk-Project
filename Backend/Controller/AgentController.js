b01c0869ad31cc366ff590b845c2ee687f870b4f
MartinaMaurice <MartinaMaurice.280@gmail.com>
2023-12-01 18:43:31 +0200

    Agent hnbd2

--- Backend/Controller/AgentController.js
+++ Backend/Controller/AgentController.js
@@ -1,6 +1,10 @@
-const express = require('express');
-const AgentModel = require('../Models/Agent');
-
-const router = express.Router();
-
-module.exports = router;
+const AgentModel = require("../Models/Agent");
+const sessionModel = require("../Models/sessionModel");
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
+const secretKey = process.env.SECRET_KEY;
+const bcrypt = require("bcrypt");
+const AgentController = {
+    
+};
+module.exports = AgentController;
