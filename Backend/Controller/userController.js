6048f00ea8b80a3aac8c1c3c52d1ae9f18745b79
amirwessam <121111555+amirwessam@users.noreply.github.com>
2023-12-06 16:14:20 +0200

    MFA:still not working 100%

--- Backend/Controller/userController.js
+++ Backend/Controller/userController.js
@@ -4,6 +4,34 @@ const jwt = require("jsonwebtoken");
 require('dotenv').config();
 const secretKey = process.env.SECRET_KEY;
 const bcrypt = require("bcrypt");
+const nodemailer = require("nodemailer");
+const speakeasy = require("speakeasy");
+const transporter = nodemailer.createTransport({
+    host: 'smtp-mail.outlook.com',
+    port: 587,
+    secure: false, // Set to true if you're using port 465 (SSL), false for port 587 (TLS)
+    auth: {
+      user: 'sehelpdeskproject@outlook.com',
+      pass: 'amirwessam2023',
+    },
+  });
+  async function sendOtpEmail(user, otp) {
+    console.log('Sending OTP email...');
+    const mailOptions = {
+        from: '"HELPDESK" <sehelpdeskproject@outlook.com>', // Replace with your email address
+        to: user.email, // User's email address
+        subject: 'Your OTP for Login',
+        text: `Your one-time password (OTP) is: ${otp}`,
+    };
+  
+    try {
+      await transporter.sendMail(mailOptions);
+      console.log('OTP email sent successfully');
+    } catch (error) {
+      console.error('Error sending email:', error);
+      throw error; // Make sure to rethrow the error to propagate it to the calling function
+    }
+  }
 const userController = {
     register: async (req, res) => {
         try {
@@ -37,54 +65,83 @@ const userController = {
     },
     login: async (req, res) => {
         try {
-            const { email, password } = req.body;
-
-            // Find the user by email
-            const user = await userModel.findOne({ email });
-
-            if (!user) {
-                return res.status(404).json({ message: "User not found" });
-            }
-
-            // Check if the password is correct
-            const passwordMatch = await bcrypt.compare(password, user.password);
-            if (!passwordMatch) {
-                return res.status(405).json({ message: "Incorrect password" });
-            }
-
-            const currentDateTime = new Date();
-            const expiresAt = new Date(+currentDateTime + 1800000); // expire in 3 minutes
-            // Generate a JWT token
-            const token = jwt.sign(
-                { user: { userId: user._id, role: user.role } },
-                secretKey,
-                {
-                    expiresIn: 3 * 60 * 60,
-                }
-            );
-
-            let newSession = new sessionModel({
-                userId: user._id,
-                token,
-                expiresAt: expiresAt,
+          const { email, password } = req.body;
+      
+          // Find the user by email
+          const user = await userModel.findOne({ email });
+      
+          if (!user) {
+            return res.status(404).json({ message: "User not found" });
+          }
+      
+          // Check password
+          const passwordMatch = await bcrypt.compare(password, user.password);
+          if (!passwordMatch) {
+            return res.status(405).json({ message: "Incorrect password" });
+          }
+      
+          const { otp } = req.body;
+      
+          if (!otp) {
+            // Generate and send new OTP to user's email in database
+            const newOtp = speakeasy.totp({
+              secret: user.secret,
+              encoding: "base32",
             });
-            await newSession.save();
-
-            return res
-                .cookie("token", token, {
-                    expires: expiresAt,
-                    withCredentials: true,
-                    httpOnly: false,
-                    sameSite: 'none'
-                })
-                .status(200)
-                .json({ message: "Login successful", user });
+            await sendOtpEmail(user, newOtp);
+      
+            // Prompt user for OTP
+            return res.status(422).json({
+              message: "OTP required for login. Check your email.",
+            });
+          }
+      
+          // Verify the provided OTP
+          const verified = speakeasy.totp.verify({
+            secret: user.secret,
+            encoding: "base32",
+            token: otp,
+          });
+      
+          if (!verified) {
+            // Invalid OTP, do not send another OTP
+            return res.status(401).json({ message: "Invalid OTP" });
+          }
+      
+          // Both password and OTP are valid, proceed to login
+          const currentDateTime = new Date();
+          const expiresAt = new Date(+currentDateTime + 1800000); // 3 minutes
+          const token = jwt.sign(
+            { user: { userId: user._id, role: user.role } },
+            secretKey,
+            {
+              expiresIn: 3 * 60 * 60,
+            }
+          );
+      
+          let newSession = new sessionModel({
+            userId: user._id,
+            token,
+            expiresAt: expiresAt,
+          });
+          await newSession.save();
+      
+          // Set cookie and respond with success
+          return res
+            .cookie("token", token, {
+              expires: expiresAt,
+              withCredentials: true,
+              httpOnly: false,
+              sameSite: "none",
+            })
+            .status(200)
+            .json({ message: "Login successful", user });
         } catch (error) {
-            console.error("Error logging in:", error);
-            res.status(500).json({ message: "Server error" });
+          console.error("Error logging in user:", error);
+          res.status(500).json({ message: "Server error" });
         }
-    },
-
+      },
+      
     getAllUsers: async (req, res) => {
         try {
             const users = await userModel.find();
