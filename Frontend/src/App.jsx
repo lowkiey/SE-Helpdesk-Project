5a61845b36e845fb042cf802aaa34f64ba6e87b5
georgeY2002 <georgeyoussef2002@gmail.com>
2023-12-09 19:05:30 +0200

    hvj

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/HomePage";
 import Login from "./pages/login";
 import Signup from "./pages/register";
-
+import knowledge from "./pages/knowledge"
 function App() {
   return (
     <Routes>
       {/* Set the login page as the default */}
       <Route path="/" element={<Login />} />
+
+
 
       {/* Other routes */}
       <Route path="/home" element={<Homepage />} />
