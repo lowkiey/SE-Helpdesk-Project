237b022867d303dd7f30640e97dd8ea91173c782
Farah Essam Abdrabo <121769716+farahessammm@users.noreply.github.com>
2023-12-12 20:12:09 +0200

    removing file

--- Frontend/src/pages/tickets.jsx
+++
@@ -1,7 +0,0 @@
-import "../stylesheets/auth.css";
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
-
-const backendUrl = "http://localhost:3000/api/v1/register";
-
