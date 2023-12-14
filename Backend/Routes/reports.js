40b23666d0418816dd1b7fbf9942786126ef74fa
Farah Essam Abdrabo <121769716+farahessammm@users.noreply.github.com>
2023-12-14 16:50:15 +0200

    Merge branch 'main' into Farah

--- Backend/Routes/reports.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const reportController = require("../Controller/reportController"); // Fix casing issue
-const authorizationMiddleware = require('../Middleware/autherizationMiddleware');
-
-router.post("/:id", authorizationMiddleware(['manager']), reportController.createReport);
-
-module.exports = router; // ! Don't forget to export the router
--- Backend/Routes/view.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const reportController = require("../Controller/reportController"); // Fix casing issue
-const authorizationMiddleware = require('../Middleware/autherizationMiddleware');
-
-router.get("/:id", authorizationMiddleware(['manager']), reportController.viewReport);
-
-module.exports = router; // ! Don't forget to export the router
