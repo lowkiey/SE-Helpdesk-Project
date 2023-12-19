a3fdce54f574ea3d27beae88307a96f6b842717c
MartinaMaurice <MartinaMaurice.280@gmail.com>
2023-12-19 22:30:49 +0200

    front end

--- Frontend/src/pages/FAQs.jsx
+++ Frontend/src/pages/FAQs.jsx
@@ -1,61 +1,103 @@
 import React, { useEffect, useState } from "react";
-// import AppNavBar from "../components/navbar";
 import axios from "axios";
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import { Link, NavLink } from 'react-router-dom';
-import { FaUser } from 'react-icons/fa';
-import { FaBell } from 'react-icons/fa';
-import { Form, FormControl, InputGroup } from 'react-bootstrap';
-import { FaSearch } from 'react-icons/fa';
-
-
-import { useNavigate } from "react-router-dom";
+import { FaBell, FaSearch, FaUser } from 'react-icons/fa';
+import { FormControl, InputGroup, Nav } from 'react-bootstrap';
+import { Link, useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
+
 let backend_url = "http://localhost:3000/api/v1";
 
 export default function HomePage() {
     const navigate = useNavigate();
     const [cookies] = useCookies([]);
     const [userName, setUserName] = useState("");
-    const [users, setUsers] = useState([]);
-    const [isUserTabOpen, setIsUserTabOpen] = useState(false)
-    const [showNotification, setShowNotification] = useState(false); // New state for notification
+    const [isUserTabOpen, setIsUserTabOpen] = useState(false);
+    const [showNotification, setShowNotification] = useState(false);
+    const [selectedCategory, setSelectedCategory] = useState(null);
+    const [selectedSubcategory, setSelectedSubcategory] = useState(null);
+    const [showHardwareDropdown, setShowHardwareDropdown] = useState(false);
+    const [showSoftwareDropdown, setShowSoftwareDropdown] = useState(false);
+    const [showNetworkDropdown, setShowNetworkDropdown] = useState(false);
 
     const handleUserIconClick = () => {
         setIsUserTabOpen(!isUserTabOpen);
     };
+
+    const handleCategoryClick = (category) => {
+        setSelectedCategory(category);
+        setSelectedSubcategory(null);
+    };
+
+    const handleSubCategoryClick = (subcategory) => {
+        setSelectedCategory(null);
+        setSelectedSubcategory(subcategory);
+    };
+
     useEffect(() => {
         async function fetchUserData() {
             try {
                 if (!cookies.token) {
                     console.log("No token found, redirecting to login");
                     navigate("/");
-                    return; // Exit early if there's no token
+                    return;
                 }
 
                 const uid = localStorage.getItem("userId");
-                console.log(uid);
-
                 const response = await axios.get(`${backend_url}/users/${uid}`, {
                     withCredentials: true,
                 });
-                console.log("response", response);
-
                 setUserName(response.data.displayName);
             } catch (error) {
                 console.log("Error fetching user data:", error);
-                navigate("/"); // Redirect to login page on error
+                navigate("/");
             }
         }
 
         fetchUserData();
     }, [cookies.token, navigate]);
-    const handleSearch = () => {
-        // Perform search functionality here
-        console.log('Perform search');
-        // Add your search logic or API call here
+
+    const customStyles = `
+    .purple-btn {
+        color: purple;
+        border-color: purple;
+    }
+
+    .custom-btn:hover,
+    .custom-btn:focus,
+    .custom-btn:active,
+    .custom-btn.active {
+        background-color: purple;
+        color: white;
+        border-color: purple;
+    }
+`;
+
+    const subcategoryInfo = {
+        "Desktops": "Information about Desktops",
+        "Laptops": "Information about Laptops",
+        "Printers": "Printers",
+	    "Servers": "Servers",
+	    "Networking Equipment": "NE",
+	    "Email Issues": "Email Issues",
+	    "Internet Connection Problems": "ICP",
+	    "Website Errors": "Errors",
+	    "Operating Systems": "OS",
+	    "Application Software": "AS",
+	    "Custom Software": "CS",
+	    "Integration Issues": "II"
+    };
+
+    const renderSubcategoryInfo = () => {
+        if (selectedSubcategory && subcategoryInfo[selectedSubcategory]) {
+            return (
+                <div className="ms-5 mt-3">
+                    <p>{subcategoryInfo[selectedSubcategory]}</p>
+                </div>
+            );
+        }
+        return null;
     };
 
     return (
@@ -65,25 +107,22 @@ export default function HomePage() {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto ms-lg-5" style={{ margin: "0px", marginLeft: '100px' }}>
-                            <Nav.Link as={Link} to="/home" style={{ fontSize: '24px', cursor: 'pointer', color: 'rgb(166, 0, 255)' }}>
+                            <Nav.Link as={Link} to="/home" style={{ fontSize: '24px', cursor: 'pointer', color: 'purple' }}>
                                 HelpDesk
                             </Nav.Link>
-                            <Nav.Link as={Link} to="/tickets" style={{ fontSize: '24px', cursor: 'pointer', color: 'rgb(166, 0, 255)', marginLeft: '50px' }}>
+                            <Nav.Link as={Link} to="/tickets" style={{ fontSize: '24px', cursor: 'pointer', color: 'purple', marginLeft: '50px' }}>
                                 Tickets
                             </Nav.Link>
-                            <Nav.Link as={Link} to="/faq" style={{ fontSize: '24px', cursor: 'pointer', color: 'rgb(166, 0, 255)', marginLeft: '50px' }}>
+                            <Nav.Link as={Link} to="/faq" style={{ fontSize: '24px', cursor: 'pointer', color: 'purple', marginLeft: '50px' }}>
                                 FAQs
                             </Nav.Link>
                         </Nav>
                         <Nav className="ms-auto" style={{ display: 'flex', alignItems: 'center' }}>
-                            {/* User Icon */}
-
-                            {/* Notification Icon */}
                             <Nav.Item>
                                 <div style={{ position: 'relative' }}>
                                     <FaBell
                                         onClick={() => setShowNotification(!showNotification)}
-                                        style={{ fontSize: '24px', cursor: 'pointer', color: 'rgb(166, 0, 255)', marginRight: '20px' }}
+                                        style={{ fontSize: '24px', cursor: 'pointer', color: 'purple', marginRight: '20px' }}
                                     />
                                     {showNotification && (
                                         <div style={{ position: 'absolute', top: '30px', right: '20px', width: '200px', height: '300px', backgroundColor: 'white', boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.2)', borderRadius: '5px', padding: '10px' }}>
@@ -99,20 +138,19 @@ export default function HomePage() {
                                 <div style={{ position: 'relative' }}>
                                     <FaUser
                                         onClick={handleUserIconClick}
-                                        style={{ fontSize: '24px', cursor: 'pointer', color: 'rgb(166, 0, 255)', marginRight: '-40px' }}
+                                        style={{ fontSize: '24px', cursor: 'pointer', color: 'purple', marginRight: '-40px' }}
                                     />
                                     {isUserTabOpen && (
                                         <div style={{ position: 'absolute', top: '30px', right: '-200px', width: '200px', height: '100px', backgroundColor: 'white', boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.2)', borderRadius: '0px', padding: '10px' }}>
                                             {/* User tab content */}
                                             <div style={{ display: 'flex', alignItems: 'center' }}>
                                                 <div style={{ marginRight: '10px', borderRadius: '50%', overflow: 'hidden' }}>
-                                                    {/* Placeholder image */}
                                                     <img src="https://via.placeholder.com/50" alt="Profile" style={{ width: '50px', height: '50px', objectFit: 'cover' }} />
                                                 </div>
                                                 <div>
-                                                    <p style={{ margin: '0', fontWeight: 'bold', marginTop: '0' }}>User Name</p>
+                                                    <p style={{ margin: '0', fontWeight: 'bold', marginTop: '0' }}>{userName}</p>
                                                     <br />
-                                                    <Link to="/" style={{ color: 'rgb(209, 151, 240)', textDecoration: 'none' }}>Logout</Link>
+                                                    <Link to="/" style={{ color: 'purple', textDecoration: 'none' }}>Logout</Link>
                                                 </div>
                                             </div>
                                         </div>
@@ -123,27 +161,92 @@ export default function HomePage() {
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-            <h1 style={{ textAlign: "left", margin: "40px", color: 'black', fontFamily: "Times New Roman", fontWeight: "bold" }}>
-                {`Hello ${userName}`} {/* Displaying the username */}
-            </h1>
-            <h3 style={{ textAlign: "left", margin: "40px", color: 'black', fontFamily: "Times New Roman", fontWeight: "lighter" }}>
-                Here you will find the Frequently Asked Questions:
+            <h3 style={{ textAlign: "left", margin: "40px", color: 'purple', fontFamily: "sans-serif", fontWeight: "bold" }}>
+                Can't find what you're looking for? Search our FAQs below!
             </h3>
             {/* Search bar */}
             <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start', marginLeft: '90px' }}>
-                <InputGroup style={{ width: '400px', height: '50px' }}>
+                <InputGroup style={{ width: '400px', height: '50px', marginTop: "-5px", marginLeft: "-45px"}}>
                     <FormControl
-                        placeholder="Search FAQ"
+                        placeholder="My Laptop Froze!"
                         aria-label="Search FAQ"
                         aria-describedby="basic-addon2"
                     />
-                    <InputGroup.Text id="basic-addon2" style={{ cursor: 'pointer' }} onClick={handleSearch}>
+                    <InputGroup.Text id="basic-addon2" style={{ cursor: 'pointer' }} onClick={() => console.log('Perform search')}>
                         <FaSearch />
                     </InputGroup.Text>
                 </InputGroup>
             </div>
-
-
-        </>
-    );
+            {/* Categories and Subcategories */}
+            <div className="d-flex flex-column align-items-start mt-4 ms-5">
+                <div className="mb-3">
+                    <span style={{fontWeight: "bold", fontFamily: "sans-serif"}}className="me-3">Categories:</span>
+                    <div className="btn-group" role="group">
+                        {/* Hardware Category */}
+                        <button style={{color: "purple", fontWeight: "bold", fontFamily: "sans-serif", borderColor: "purple", borderWidth: "2px", borderRadius: "5px"}}
+                            onMouseEnter={() => setShowHardwareDropdown(true)}
+                            onMouseLeave={() => setShowHardwareDropdown(false)}
+                            type="button"
+                            className={`btn btn-outline-primarypurple-btn ${selectedCategory === "Hardware" ? "active" : ""}`}
+                            onClick={() => handleCategoryClick("Hardware")}
+                        >
+                            Hardware
+                        </button>
+                        {/* Hardware Subcategories Dropdown */}
+                        <div style={{marginTop:"35px", color: "purple", fontWeight: "bold", fontFamily: "sans-serif", borderColor: "purple", borderWidth: "2px", borderRadius: "5px"}}  className={`dropdown-menu ${showHardwareDropdown ? "show" : ""}`} onMouseEnter={() => setShowHardwareDropdown(true)} onMouseLeave={() => setShowHardwareDropdown(false)}>
+                            <button onClick={() => handleSubCategoryClick("Desktops")} className="dropdown-item" type="button">Desktops</button>
+                            {/* Add other Hardware subcategories */}
+                            <button onClick={() => handleSubCategoryClick("Laptops")} className="dropdown-item" type="button">Laptops</button>
+                            <button onClick={() => handleSubCategoryClick("Printers")} className="dropdown-item" type="button">Printers</button>
+                            <button onClick={() => handleSubCategoryClick("Servers")} className="dropdown-item" type="button">Servers</button>
+                            <button onClick={() => handleSubCategoryClick("Networking Equipment")} className="dropdown-item" type="button">Network Equipment</button>
+                        </div>
+                    </div>
+                    <div className="btn-group" role="group">
+                        {/* Software Category */}
+                        <button style={{color: "purple", fontWeight: "bold", fontFamily: "sans-serif", borderColor: "purple", borderWidth: "2px", borderRadius: "5px"}}
+                            onMouseEnter={() => setShowSoftwareDropdown(true)}
+                            onMouseLeave={() => setShowSoftwareDropdown(false)}
+                            type="button"
+                            className={`btn btn-outline-primarypurple-btn ${selectedCategory === "Software" ? "active" : ""}`}
+                            onClick={() => handleCategoryClick("Software")}
+                        >
+                            Software
+                        </button>
+                        {/* Software Subcategories Dropdown */}
+                        <div style={{marginTop:"35px", color: "purple", fontWeight: "bold", fontFamily: "sans-serif", borderColor: "purple", borderWidth: "2px", borderRadius: "5px"}} className={`dropdown-menu ${showSoftwareDropdown ? "show" : ""}`} onMouseEnter={() => setShowSoftwareDropdown(true)} onMouseLeave={() => setShowSoftwareDropdown(false)}>
+                            <button onClick={() => handleSubCategoryClick("Operating Systems")} className="dropdown-item" type="button">Operating System</button>
+                            {/* Add other Software subcategories */}
+                            <button onClick={() => handleSubCategoryClick("Application Software")} className="dropdown-item" type="button">Application Software</button>
+                            <button onClick={() => handleSubCategoryClick("Custom Software")} className="dropdown-item" type="button">Custom Software</button>
+                            <button onClick={() => handleSubCategoryClick("Integration Issues")} className="dropdown-item" type="button">Integration Issues</button>
+                        </div>
+                    </div>  
+                         {/* Network Category */}
+                            <div className="btn-group" role="group">
+                         <button style={{color: "purple", fontWeight: "bold", fontFamily: "sans-serif", borderColor: "purple", borderWidth: "2px", borderRadius: "5px"}}
+                            onMouseEnter={() => setShowNetworkDropdown(true)}
+                            onMouseLeave={() => setShowNetworkDropdown(false)}
+                            type="button"
+                            className={`btn btn-outline-primarypurple-btn ${selectedCategory === "Network" ? "active" : ""}`}
+                            onClick={() => handleCategoryClick("Network")}
+                        >
+                            Network
+                        </button>
+                        {/* Network Subcategories Dropdown */}
+                        <div style={{marginTop:"35px", color: "purple", fontWeight: "bold", fontFamily: "sans-serif", borderColor: "purple", borderWidth: "2px", borderRadius: "5px"}} className={`dropdown-menu ${showNetworkDropdown ? "show" : ""}`} onMouseEnter={() => setShowNetworkDropdown(true)} onMouseLeave={() => setShowNetworkDropdown(false)}>
+                            <button onClick={() => handleSubCategoryClick("Email Issues")} className="dropdown-item" type="button">Email Issues</button>
+                            {/* Add other Software subcategories */}
+                            <button onClick={() => handleSubCategoryClick("Internet Connection Problems")} className="dropdown-item" type="button">Internet Connection Problems</button>
+                            <button onClick={() => handleSubCategoryClick("Website Errors")} className="dropdown-item" type="button">Website Errors</button>
+                        </div>                      
+                 </div>
+                 </div>
+            </div>
+            {/* Display Selected Subcategory Info */}
+            {renderSubcategoryInfo()}
+        </>  
+    );  
 }
+
+		
--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -59,13 +59,13 @@ export default function HomePage() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto ms-lg-5" style={{ margin: "0px", marginLeft: '100px' }}>
-              <Nav.Link as={Link} to="/home" style={{ fontSize: '24px', cursor: 'pointer', color: 'rgb(166, 0, 255)' }}>
+              <Nav.Link as={Link} to="/home" style={{ fontSize: '24px', cursor: 'pointer', color: 'purple' }}>
                 HelpDesk
               </Nav.Link>
-              <Nav.Link as={Link} to="/tickets" style={{ fontSize: '24px', cursor: 'pointer', color: 'rgb(166, 0, 255)', marginLeft: '50px' }}>
+              <Nav.Link as={Link} to="/tickets" style={{ fontSize: '24px', cursor: 'pointer', color: 'purple', marginLeft: '50px' }}>
                 Tickets
               </Nav.Link>
-              <Nav.Link as={Link} to="/faq" style={{ fontSize: '24px', cursor: 'pointer', color: 'rgb(166, 0, 255)', marginLeft: '50px' }}>
+              <Nav.Link as={Link} to="/faq" style={{ fontSize: '24px', cursor: 'pointer', color: 'purple', marginLeft: '50px' }}>
                 FAQs
               </Nav.Link>
             </Nav>
@@ -77,7 +77,7 @@ export default function HomePage() {
                 <div style={{ position: 'relative' }}>
                   <FaBell
                     onClick={() => setShowNotification(!showNotification)}
-                    style={{ fontSize: '24px', cursor: 'pointer', color: 'rgb(166, 0, 255)', marginRight: '20px' }}
+                    style={{ fontSize: '24px', cursor: 'pointer', color: 'purple', marginRight: '20px' }}
                   />
                   {showNotification && (
                     <div style={{ position: 'absolute', top: '30px', right: '20px', width: '200px', height: '300px', backgroundColor: 'white', boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.2)', borderRadius: '5px', padding: '10px' }}>
@@ -93,7 +93,7 @@ export default function HomePage() {
                 <div style={{ position: 'relative' }}>
                   <FaUser
                     onClick={handleUserIconClick}
-                    style={{ fontSize: '24px', cursor: 'pointer', color: 'rgb(166, 0, 255)', marginRight: '-40px' }}
+                    style={{ fontSize: '24px', cursor: 'pointer', color: 'purple', marginRight: '-40px' }}
                   />
                   {isUserTabOpen && (
                     <div style={{ position: 'absolute', top: '30px', right: '-200px', width: '200px', height: '100px', backgroundColor: 'white', boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.2)', borderRadius: '0px', padding: '10px' }}>
@@ -124,12 +124,24 @@ export default function HomePage() {
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         <button className="create" onClick={() => navigate("/tickets")} style={{ fontFamily: "sans-serif", fontWeight: "bold", backgroundColor: 'purple', color: 'white', border: 'white', borderRadius: '5px', width: '15%', padding: '8px', }}>Create a new support ticket</button><button className="activity" onClick={() => navigate("/tickets")} style={{ marginTop: '25px', fontFamily: "sans-serif", fontWeight: "bold", backgroundColor: 'purple', color: 'white', border: 'white', borderRadius: '5px', width: '15%', padding: '8px', }}>Recent Activity</button>
       </div>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
+    <img
+    src="Frontend/public/gg.jpg"
+    alt="Image Description"
+  />
+    </div>
       <div className="about us">
-        <h1 style={{ textAlign: "center", marginTop: '90px', color: 'purple', fontFamily: "Sans-Serif", fontWeight: "bold", marginLeft: 'auto', marginRight: 'auto', width: 'fit-content' }}>
+        <h1 style={{ textAlign: "left", marginTop: '90px', color: 'purple', fontFamily: "Sans-Serif", fontWeight: "bold", marginLeft: '35px', marginRight: 'auto', width: 'fit-content' }}>
           About Us
         </h1>
-        <p style={{ textAlign: "center", marginTop: '10px', marginRight: "1650px", color: 'purple', fontFamily: "Sans-Serif", fontWeight: "bold", width: '1500px', padding: '20px' }}>
+        <p style={{ textAlign: "left", marginTop: '10px', marginLeft: "15px", color: 'black', fontFamily: "Sans-Serif", fontWeight: "bold", width: '1500px', padding: '20px' }}>
           Our mission at Helpdesk is to revolutionize support services through our cutting-edge ticketing system. We are dedicated to streamlining communication between users and support teams, ensuring swift and efficient resolution of issues. Our commitment lies in empowering both clients and businesses by providing a seamless, user-friendly platform that fosters clarity, transparency, and satisfaction. We strive to be the catalyst for exceptional customer experiences, offering reliable solutions that elevate the standards of support services in the digital age.
+        </p>
+        <h1 style={{ textAlign: "left", marginTop: '20px', color: 'purple', fontFamily: "Sans-Serif", fontWeight: "bold", marginLeft: '35px', marginRight: 'auto', width: 'fit-content' }}>
+          Our Services
+        </h1>
+        <p style={{ textAlign: "left", marginTop: '10px', marginLeft: "15px", color: 'black', fontFamily: "Sans-Serif", fontWeight: "bold", width: '1500px', padding: '20px' }}>
+        Our services are tailored to provide efficient solutions for your needs. Need help? Simply open a ticket with us, and rest assured that a specialized agent, skilled in hardware, software, or network-related issues, will promptly address your concern. Our priority ticketing system ensures that urgent matters receive immediate attention, while our live chat functionality enables real-time communication for quick resolutions. Experience reliable support catered specifically to your technical requirements, making your experience with us smooth and hassle-free.
         </p>
       </div>
 
